29e60c4f88c750c218f546efa03b0809e37ef0ed
Rocillo <guilherme.rocillo@sp.senai.br>
2022-06-20 21:27:28 -0300

    Atualização 20/06/22

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './clientes/formulario/formulario.component';
+import { HomeComponent } from './home/home.component';
+import { JogosComponent } from './jogos/jogos.component';
 
-const routes: Routes = [];
+const routes: Routes = [  {
+  path:"home",
+  component: HomeComponent
+},
+{
+  path:'jogos',
+  component: JogosComponent
+},
+{
+  path:"formulario",
+  component: FormularioComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
